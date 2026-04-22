9510cbbde2f7d7f436a0ee660d9745f8769f4530
João Vassalo <jvassalo@BALD154705.corp.udesc.br>
2026-04-22 10:48:54 -0300

    Stable version using moment based, most efficient memory organization until here.

--- functions/grid_plot.cuh
+++ functions/grid_plot.cuh
@@ -2,8 +2,18 @@
 
 #include "../presets/geometry.h"
 #include "../presets/config.h"
+#include "rest.cuh"
 
 __host__ inline int grid_plot(int x, int y)
 {
-    return (((x / BX) + GX * (y / BY)) * (BX * BY) + ((x - BX * (x / BX)) + BX * (y - BY * (y / BY))));
+    int blockX = x / BX;
+    int blockY = y / BY;
+
+    int localX = rest(x, BX);
+    int localY = rest(y, BY);
+
+    int blockId = blockX + GX * blockY;
+    int localId = localX + BX * localY;
+
+    return blockId * (BX * BY) + localId;
 }
