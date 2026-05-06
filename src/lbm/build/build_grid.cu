7ffc61b7841eca72f10f4acb97d711f5c73c0ce4
João Vassalo <jvassalo@BALD154705.corp.udesc.br>
2026-05-06 15:25:09 -0300

    Working on better methods of labeling the grid

--- src/lbm/build/build_grid.cu
+++ src/lbm/build/build_grid.cu
@@ -8,6 +8,18 @@ struct Grid2D
     uint8_t *node;
 };
 
-void build_grid()
+__host__ void build_grid()
 {
+
+    Grid2D grid;
+
+    cudaMalloc((void **)grid.mask, sizeof(uint32_t) * Nx * Ny);
+    cudaMalloc((void **)grid.node, sizeof(uint8_t) * Nx * Ny);
+}
+
+__device__ void init_grid()
+{
+
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
 }
